720eee9aa7188cbefe83ad1911182c65f8f12695
Kane Kabena <onekaneldn@gmail.com>
2025-03-05 10:28:10 +0300

    Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+console.log(scuberGreetingForFeet(2000));
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+  if (city === "NYC")
+    return "Ok, sounds good.";
+  else (city !== "NYC")
+    return "No go.";
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-}
+  switch(tip) {
+   case "generous":
+    return "Thank you so much.";
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  case "not as generous":
+    return "Thank you.";
+    
+  default:
+    return "Bye.";
+  }
 }
